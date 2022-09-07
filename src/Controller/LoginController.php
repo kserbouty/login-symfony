7ab6e4a8085b44fb1620f5a31f3bbf92e8f3d762
Karim Serbouty <kserbouty.dev@protonmail.com>
2022-09-07 13:56:09 +0200

    Add logout on auth view

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -12,9 +12,9 @@ class LoginController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        /*if ($this->getUser()) {
+        if ($this->getUser()) {
             return $this->redirectToRoute('app_devboard');
-        }*/
+        }
 
         $error = $authenticationUtils->getLastAuthenticationError();
         $last_username = $authenticationUtils->getLastUsername();
